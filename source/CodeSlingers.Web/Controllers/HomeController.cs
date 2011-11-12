a7461fdcfbca53dd5d6cd5a86156d0df9972a93f
Ryan Tofteland <ryantofteland@gmail.com>
2011-11-12 12:04:38 -0600

    adding Autofac hookups

--- source/CodeSlingers.Web/Controllers/HomeController.cs
+++ source/CodeSlingers.Web/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CodeSlingers.Web.Services;
 
 namespace CodeSlingers.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDummyService _dummyService;
+
+        public HomeController(IDummyService dummyService)
+        {
+            _dummyService = dummyService;
+        }
+
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
---
+++ source/CodeSlingers.Web/Services/DummyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSlingers.Web.Services
+{
+    public interface IDummyService : IService
+    {
+
+    }
+
+    public class DummyService : IDummyService
+    {
+
+    }
+}
