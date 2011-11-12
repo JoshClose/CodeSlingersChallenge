4a2d6ec66cca26d37d9c588b51ba7c717107f979
Eric <eric.brandes@gmail.com>
2011-11-12 14:09:01 -0600

    Adding more stuff

--- source/CodeSlingers.Web/Models/Business.cs
+++ source/CodeSlingers.Web/Models/Business.cs
@@ -7,5 +7,22 @@ namespace CodeSlingers.Web.Models
 {
     public class Business
     {
+        public int Id { get; set; }
+
+        public long Latitude { get; set; }
+
+        public long Longitude { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Category { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
---
+++ source/CodeSlingers.Web/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSlingers.Web.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public IList<Wine> Wines { get; set; }
+    }
+}
