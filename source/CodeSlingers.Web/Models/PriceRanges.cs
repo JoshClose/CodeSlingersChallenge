ff4e6c46c3ece74626c7c2e02f6e5d5aa9ded430
Ryan Tofteland <ryantofteland@gmail.com>
2011-11-12 18:06:24 -0600

    Adding some data for wines

--- source/CodeSlingers.Web/Models/PriceRanges.cs
+++ source/CodeSlingers.Web/Models/PriceRanges.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace CodeSlingers.WP7.App.Models
+namespace CodeSlingers.Web.Models
 {
     public static class PriceRanges
     {
